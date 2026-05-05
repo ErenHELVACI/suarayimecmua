8ed086c5b2f931cd7b8c1187f17128162b7f5d99
Eren HELVACI <151572076+ErenHELVACI@users.noreply.github.com>
2026-05-05 18:55:35 +0300

    e

--- main.js
+++ main.js
@@ -2335,7 +2335,7 @@ function showPoetSkeletons(container, count = 8) {
 // --- PWA SERVICE WORKER REGISTRATION ---
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then(reg => console.log('Service Worker Kayıt Başarılı:', reg.scope))
             .catch(err => console.log('Service Worker Kayıt Hatası:', err));
     });
--- sw.js
+++ sw.js
@@ -3,7 +3,9 @@ const ASSETS = [
   'index.html',
   'style.css',
   'main.js',
-  'manifest.json'
+  'manifest.json',
+  'assets/icon-192.png',
+  'assets/icon-512.png'
 ];
 
 // Yükleme sırasında dosyaları önbelleğe al
